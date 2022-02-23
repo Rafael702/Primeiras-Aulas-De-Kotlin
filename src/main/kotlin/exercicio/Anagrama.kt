9fea7b88b76fb77a5b43c3d69d7b8de94984ce7a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-23 14:54:08 -0300

    Concluir busca de anagrama para palavra estatica.

---
+++ src/main/kotlin/exercicio/Anagrama.kt
@@ -0,0 +1,65 @@
+package src.exercicio
+
+
+fun buscarAnagrama(palavra: String): String {
+
+    val listaDePalavras: Array<String> = arrayOf("ROMA", "SAL", "COBRA", "BROCA")
+
+    palavra.forEach { i ->
+        val anagrama: String = verificarPalavraNaListaDeAnagrama(listaDePalavras, i, palavra)
+        val temAnagrama: Boolean = anagrama.isNotEmpty()
+        if (temAnagrama) {
+            return "O Anagrama de $palavra É: $anagrama"
+        }
+    }
+
+    return "Nao Foi Encontrado Anagrama Para Esta Palavra"
+}
+
+fun verificarPalavraNaListaDeAnagrama(listaDePalavras: Array<String>, caracterDePalavra: Char, palavra: String): String {
+    val anagramaFormado: String = ""
+
+    listaDePalavras.forEach { anagrama ->
+        val anagramaComValidacao = validarAnagrama(anagrama, caracterDePalavra, palavra, anagramaFormado)
+        when {
+            anagramaComValidacao.isNotEmpty() -> {
+                return anagramaComValidacao
+            }
+        }
+    }
+    return ""
+}
+
+fun validarAnagrama(anagrama: String, caracterDePalavra: Char, palavra: String, anagramaFormado: String): String {
+    val listaComAnagramasDaPalavra: ArrayList<String> = arrayListOf()
+
+    var anagramaValidado = anagramaFormado
+
+    anagrama.forEach { a ->
+        if (anagrama.contains(caracterDePalavra)) if (palavra.contains(a)) {
+            anagramaValidado += a
+            if (anagramaValidado == anagrama)
+                listaComAnagramasDaPalavra.add(anagramaValidado)
+        }
+    }
+    return apresentarAnagramasDaPalavra(listaComAnagramasDaPalavra)
+}
+
+fun apresentarAnagramasDaPalavra(listaComAnagramasDaPalavra: ArrayList<String>): String {
+    var anagramasDaLista: String = ""
+
+    listaComAnagramasDaPalavra.forEach { anagramas ->
+        anagramasDaLista += "$anagramas \n"
+    }
+    return anagramasDaLista
+}
+
+fun main() {
+
+    val palavra = "Barco"
+
+    println(buscarAnagrama(palavra.uppercase()))
+
+    println("FIm")
+
+}
