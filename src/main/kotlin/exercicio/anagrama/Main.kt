5f48a6a8175b02764f4102972dc59b098dfc706e
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-03-01 20:40:13 -0300

    Criar classe Main com a funcao main.

---
+++ src/main/kotlin/exercicio/anagrama/Main.kt
@@ -0,0 +1,13 @@
+package exercicio.anagrama
+
+import src.exercicio.Anagrama
+
+class Main
+fun main() {
+
+    println("Bem vindo ao programa de Anagramas!!!")
+    println("Para começarmos, digite uma palavra e lhe apresentaremos os anagrama:")
+    val palavra = readLine() ?: "ola mundo"
+    print(Anagrama().buscarAnagrama(palavra.uppercase())
+ )
+}
