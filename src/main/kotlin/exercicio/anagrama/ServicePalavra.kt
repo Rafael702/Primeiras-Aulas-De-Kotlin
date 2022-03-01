055f259bb5d8522ca4f2b5ee45ed73a1207da830
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-03-01 20:43:12 -0300

    Criar classe Service Palavra.

---
+++ src/main/kotlin/exercicio/anagrama/ServicePalavra.kt
@@ -0,0 +1,18 @@
+package src.exercicio.anagrama
+
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ServicePalavra {
+    fun listarPalavrasParaAnagrama(): Array<String> {
+        val path: Path = Paths.get(
+            "C:\\Users\\rafael.almeida\\Desktop\\Aulas - ZUP\\Aulas\\Aulas-Kotlin\\src\\main\\kotlin\\exercicio",
+            "text.txt"
+        )
+
+        val listaDePalavras: Array<String> = Files.readAllLines(path).toTypedArray()
+
+        return listaDePalavras
+    }
+}
