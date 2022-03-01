746fc0a1bdf4ad9dd7a6144725342e89496dd91c
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-03-01 20:44:24 -0300

    Indentar codigo.

--- src/main/kotlin/exercicio/anagrama/Main.kt
+++ src/main/kotlin/exercicio/anagrama/Main.kt
@@ -3,11 +3,13 @@ package exercicio.anagrama
 import src.exercicio.Anagrama
 
 class Main
+
 fun main() {
 
     println("Bem vindo ao programa de Anagramas!!!")
     println("Para começarmos, digite uma palavra e lhe apresentaremos os anagrama:")
     val palavra = readLine() ?: "ola mundo"
-    print(Anagrama().buscarAnagrama(palavra.uppercase())
- )
+    print(
+        Anagrama().buscarAnagrama(palavra.uppercase())
+    )
 }
