658adc2598a8d650d88e96e5597462779ff11bed
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-03-04 15:59:41 -0300

    Finalizar main.

--- src/main/kotlin/exercicio/anagrama/Main.kt
+++ src/main/kotlin/exercicio/anagrama/Main.kt
@@ -1,16 +1,11 @@
 package exercicio.anagrama
 
-import src.exercicio.Anagrama
 import src.exercicio.anagrama.ServiceAnagrama
 
 class Main
 
 fun main() {
 
-    println("Bem vindo ao programa de Anagramas!!!")
-    println("Para começarmos, digite uma palavra e lhe apresentaremos os anagrama:")
-    val palavra = readLine() ?: "ola mundo"
-    print(
-        ServiceAnagrama().buscarAnagrama(palavra)
-    )
+    print(ServiceAnagrama().pegarPalavraInseridaPeloUsuario())
+
 }
